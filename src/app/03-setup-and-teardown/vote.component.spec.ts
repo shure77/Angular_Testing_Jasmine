c78742fea46f8bda09e8e77ee8da8c6b5d4bb0cb
Thaler <Roman.Thaler@adesso.at>
2019-08-19 10:53:56 +0200

    testing of components

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,30 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
+    // TESTING A COMPONENT!
+    // ARRANGE ACT ASSERT (Behaupten)
 
 describe('VoteComponent', () => {
-  it('', () => {
+
+  let component: VoteComponent;
+
+  // before Each test, we create the component, otherwise the second test would fail, because totalVotes would not be -1 but 0
+  // also beforeAll() and afterEach() functions exist
+  // Arrange, component is initialized
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+
+    // Act, call the components method
+    component.upVote();
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    
+    component.downVote();
+    
+    expect(component.totalVotes).toBe(-1);
   });
 });
