70873f7aaf55f1120c5bdc66996b3404e9a87098
Thaler <Roman.Thaler@adesso.at>
2019-08-19 11:18:07 +0200

    test event emitter

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,21 @@
 import { VoteComponent } from './vote.component'; 
+// test if event is emitted
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  let component: VoteComponent; 
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    let totalVotes = null;
+    component.voteChanged.subscribe( tv => totalVotes = tv);
+
+    component.upVote();
+
+    //expect(totalVotes).not.toBeNull(); // expect totalVotes not to be null - not specific enough
+    expect(totalVotes).toBe(1);
+
   });
 });
